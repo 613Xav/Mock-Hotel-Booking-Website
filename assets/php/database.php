bcab634a128fa919962a4b49d6088b8ff69cbdee
Xavier Darboux <86631670+613Xav@users.noreply.github.com>
2025-03-21 10:37:09 -0400

    Initial commit

---
+++ assets/php/database.php
@@ -0,0 +1,20 @@
+<?php
+
+try
+{
+    // Connects to the database
+    $db = new PDO("mysql:host=localhost;dbname=gestionhoteliere", "root", "");
+
+    // Set the PDO error mode to exception
+    $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo 'connected';
+    
+}
+catch (PDOException $error)
+{
+   /* If there is an error an exception is thrown */
+   echo 'Database connection failed.' . $error;
+   die();
+}
+?>

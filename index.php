eb4d6cb1ad3dce57c5edd454209aeccd38138065
IvanSawaya <ivansawaya@gmail.com>
2025-03-31 11:45:36 -0400

    Added reservation to rent, view updates

--- index.php
+++ index.php
@@ -58,7 +58,6 @@ require_once "assets/php/database.php";
 							<header>
 								<h1>You are currently on the E-Hotels client view<br />
 								EClick here for the <a href="employee-view.php">employee View </a>.</h1>
-								<p>Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod sit amet nisi euismod sed cursus arcu elementum ipsum arcu vivamus quis venenatis orci lorem ipsum et magna feugiat veroeros aliquam. Lorem ipsum dolor sit amet nullam dolore.</p>
 							</header>
 							<section class="tiles">
 								<article class="style1">
